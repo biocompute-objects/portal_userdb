66f87380afc27d898fd3b96328c066fa22fc293e
hadleyking <hadley_king@gwu.edu>
2023-03-01 12:50:08 -0500

    add button Changes to be committed: 	modified:   client/src/components/builder/index.js 	modified:   client/src/components/builder/preview.js 	modified:   client/src/services/bco.service.js 	modified:   client/src/slices/bcoSlice.js

--- client/src/services/bco.service.js
+++ client/src/services/bco.service.js
@@ -48,11 +48,61 @@ const createDraftBco = async (bcoURL, bcoObject) => {
   return response;
 }
 
+const updateDraftBco = async (bcoURL, bcoObject) => {
+  const response = await axios.post(`${bcoURL}objects/drafts/modify/`, {
+    "POST_api_objects_drafts_modify": [
+      {
+        "object_id": bcoObject["object_id"],
+        "contents": 
+          bcoObject
+      }
+    ]
+  }, {
+    headers: {
+      "Authorization": `Token ${JSON.parse(localStorage.getItem("user"))["bcodbs"][0]["token"]}`,
+      "Content-Type": "application/json"
+    }
+  });
+  return response;
+}
+
+const publishDraftBco = async (bcoURL, bcoObject) => {
+  const response = await axios.post(`${bcoURL}objects/drafts/publish/`, {
+    "POST_api_objects_drafts_modify": [
+      {
+        "object_id": bcoObject["object_id"],
+        "contents": 
+          bcoObject
+      }
+    ]
+  }, {
+    headers: {
+      "Authorization": `Token ${JSON.parse(localStorage.getItem("user"))["bcodbs"][0]["token"]}`,
+      "Content-Type": "application/json"
+    }
+  });
+  return response;
+}
+
+const validateBco = async (bcoURL, bcoObject) => {
+  const response = await axios.post(`${bcoURL}objects/validate/`, {
+    "POST_validate_bco": [bcoObject]
+  }, {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+  return response;
+}
+
 const BcoService = {
   addExtension,
   getDraftBco,
   getPubBco,
   createDraftBco,
+  updateDraftBco,
+  publishDraftBco,
+  validateBco,
 };
 
 export default BcoService;
