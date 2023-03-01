0664a992e7815466ddb36e680189208f0186e1c2
hadleyking <hadley_king@gwu.edu>
2023-03-01 12:50:36 -0500

    s

--- client/src/components/builder/preview.js
+++ client/src/components/builder/preview.js
@@ -2,25 +2,41 @@ import React, { useState } from "react";
 import { Button, Card, CardContent, Grid, Paper, Typography } from "@material-ui/core";
 import { useSelector, useDispatch } from "react-redux";
 import ReactJson from "react-json-view"
-import { createDraftBco, updateDraftBco } from "../../slices/bcoSlice";
+import { createDraftBco, updateDraftBco, publishDraftBco, validateBco } from "../../slices/bcoSlice";
 
 export const Preview = () => {
   const bco = useSelector(state => state.bco.data);
+  const valid = useSelector(state => state.bco.status);
   const dispatch = useDispatch();
   const BCODB_URL = process.env.REACT_APP_BCOAPI_URL;
 
-  const saveDraft = () => {
-    console.log("Preview", BCODB_URL, bco)
+  const createDraft = () => {
+    console.log("create", BCODB_URL, bco)
     const bcoURL = BCODB_URL
     const bcoObject = bco
     dispatch(createDraftBco({bcoURL, bcoObject}))
   }
 
   const updateDraft = () => {
-    console.log("Preview", BCODB_URL, bco)
+    console.log("Update", BCODB_URL, bco)
     const bcoURL = BCODB_URL
     const bcoObject = bco
     dispatch(updateDraftBco({bcoURL, bcoObject}))
+  }
+
+  const publish = () => {
+    console.log("Publish", BCODB_URL, bco)
+    const bcoURL = BCODB_URL
+    const bcoObject = bco
+    dispatch(publishDraftBco({bcoURL, bcoObject}))
+  }
+
+  const validate = () => {
+    console.log("Validate", BCODB_URL, bco)
+    const bcoURL = BCODB_URL
+    const bcoObject = bco
+    dispatch(validateBco({bcoURL, bcoObject}))
+    
   }
 
   return (
@@ -47,13 +63,23 @@ export const Preview = () => {
                 type='submit'
                 variant="contained"
                 color="primary"
-                onClick={() =>  saveDraft()  }
+                onClick={() =>  createDraft()  }
               > Save as Draft </Button>
             </Grid>)
           }
           <Grid item xs>
             <Button
+              id="validate"
               type='submit'
+              variant="contained"
+              color="primary"
+              onClick={() => validate()}
+            > Validate BCO</Button>
+          </Grid>
+          <Grid item xs>
+            <Button
+              type='submit'
+              disabled={valid !== true}
               variant="contained"
               color="secondary"
               onClick={() =>  publish()  }
