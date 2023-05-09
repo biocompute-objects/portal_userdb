0217c2669181041e53734a58a7a3c30ee8a41dcd
hadleyking <hadley_king@gwu.edu>
2023-05-09 18:00:45 -0400

    Add Orcid User Info Api
    Fix https://github.com/biocompute-objects/playbook-partnership/issues/16
    Changes to be committed:
    	modified:   server/authentication/apis.py
    	modified:   server/authentication/services.py
    	modified:   server/authentication/urls.py

--- server/authentication/urls.py
+++ server/authentication/urls.py
@@ -4,7 +4,7 @@ from rest_framework_jwt.views import (
     refresh_jwt_token,
     verify_jwt_token,
 )
-from authentication.apis import GoogleLoginApi, GoogleRegisterApi, OrcidLoginApi
+from authentication.apis import GoogleLoginApi, GoogleRegisterApi, OrcidLoginApi, OrcidUserInfoApi
 from users.apis import UserCreateApi
 
 urlpatterns = [
@@ -15,6 +15,7 @@ urlpatterns = [
     path("google/login/", GoogleLoginApi.as_view()),
     path("google/register/", GoogleRegisterApi.as_view()),
     path("orcid/login/", OrcidLoginApi.as_view()),
+    path("orcid/user_info/", OrcidUserInfoApi.as_view()),
     # path("orcid/register/", GoogleRegisterApi.as_view()),
     path("password_reset/", include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
