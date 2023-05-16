a48a00ce71d204e658880c6b6427d8152599a327
tianywan819 <57418894+tianywan819@users.noreply.github.com>
2023-05-16 13:56:04 -0400

    GA4 added

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,14 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./routes";
+import TagManager from "react-gtm-module";
 
-
+function initializeReactGA() {
+    TagManager.initialize({gtmId:'GTM-TD6W6Q2'});
+  }
 
 function App() {
+  initializeReactGA();
   return (
     <BrowserRouter>
       <div className="App">
