491376dff69469a771fbf491d444cda4d3fbd3e7
hadleyking <hadley_king@gwu.edu>
2023-12-19 14:25:37 -0500

    CSS and organization fixes Changes to be committed: 	modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/App.css 	modified:   client/src/App.js 	modified:   client/src/components/about/BCOnexus.js 	modified:   client/src/components/about/MeetOurTeam.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,21 @@ import { BrowserRouter } from "react-router-dom";
 import Router from "./routes";
 import ReactGA from "react-ga4";
 
-function initializeReactGA(){
-  ReactGA.initialize("G-RP8P4D7VWX");
-  ReactGA.send({ hitType: "pageview", page: global.window.location.pathname });
+function initializeReactGA() {
+  const version = new Date().toISOString(); // Use the current timestamp as the version
+  const gaTrackingId = "G-RP8P4D7VWX";
+
+  ReactGA.initialize(gaTrackingId, { gaOptions: { storage: "none" } });
+  ReactGA.send({
+    hitType: "pageview",
+    page: global.window.location.pathname,
+  });
+
+  // Load the script with a version in the query string
+  const script = global.document.createElement("script");
+  script.src = `https://www.google-analytics.com/ga.js?v=${version}`;
+  script.async = true;
+  global.document.head.appendChild(script);
 }
 
 function App() {
