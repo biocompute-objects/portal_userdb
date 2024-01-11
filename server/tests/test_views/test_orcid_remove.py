66cfc6c3f23c00dfe8f78500b75ec35d0e770d51
hadleyking <hadley_king@gwu.edu>
2024-01-11 11:32:40 -0500

    Add tests for new APIs and Swagger Docs
    - /orcid/add/
    - /orcid/remove/
    Changes to be committed:
    	modified:   server/authentication/apis.py
    	new file:   server/tests/test_views/test_orcid_add.py
    	modified:   server/tests/test_views/test_orcid_auth.py
    	new file:   server/tests/test_views/test_orcid_remove.py
    	modified:   server/users/selectors.py

---
+++ server/tests/test_views/test_orcid_remove.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""Test Orcid Remove API [WIP]
+
+Test for '200: Remove ORCID successful.', '401: Unathorized.', and '403: Bad request.'
+"""
+
+from rest_framework.test import APIClient, APITestCase
+from users.models import User
+
+class TestRemoveOrcidApi(APITestCase):
+    """Class for testing Remove ORCID API
+    """
+
+    fixtures = ['tests/fixtures/testing_data']
+    def setUp(self):
+        self.client = APIClient()
+    
+    def test_orcid_remove_unathorized(self):
+        """Test for ORCID remove '401: Unathorized.'
+        """
+    
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Bearer TEST',
+            HTTP_ORIGIN='http://testing.org'
+        )
+        response = self.client.post("/users/orcid/remove/")
+        
+        self.assertEqual(response.status_code, 401)
--- server/users/selectors.py
+++ server/users/selectors.py
@@ -5,24 +5,28 @@ from users.models import Profile
 
 def user_from_email(email: str) -> User:
     """User from email
+    
     returns a user object from an email
     """
     return User.objects.get(email=email)
 
 def user_from_username(username: str) -> User:
     """User from Username
+    
     returns a user object from a username
     """
     return User.objects.get(username=username)
 
 def profile_from_username(username: str) -> Profile:
     """Profile from Username
+
     returns a user object from a username
     """
     return Profile.objects.get(username=username)
 
 def user_from_orcid(orcid: str):
     """User from Orcid
+
     returns a user object from an ORCID
     """
     try:
