36236259148f869c9ea095e2ff1238231e6d0e3c
Tianyi Wang <90009613+tiwa1154@users.noreply.github.com>
2024-06-10 16:58:13 -0400

    Update settings.py
    test secret

--- server/config/settings.py
+++ server/config/settings.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from datetime import timedelta
 from django.core.management.utils import get_random_secret_key
 
+
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
